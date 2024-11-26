25ac613496f51e7d3cc09efbded842484fd71cf8
Mattia-f-4 <mattia.favretto04@gmail.com>
2024-11-26 09:14:42 +0100

    Add files via upload
    Creazione del main esclusi i 2 costruttori di copia, per i quali serve una funzione

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "LidarDriver.h"
+#include "Invalid.h"
 #include <vector>
 #include <string>
 
@@ -19,17 +20,78 @@ int main()
         v2.push_back(i);
     }
 
+    //Vettore con 11 elementi
+    vector<double> v3{1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0};
+
     
 
     //Costruttore 1 parametro
     LidarDriver lidar1(1.0);
 
-    std::cout << lidar1;
+    cout << lidar1 << std::endl;
 
-    LidarDriver lidar2(0.5);
+    lidar1.new_scan(v1);
 
-    LidarDriver lidar3(lidar1);
+    cout<<lidar1 << std::endl;
+
+    lidar1.new_scan(v2);
     
+    cout<<lidar1 << std::endl;
+
+    lidar1.new_scan(v3);
+
+    cout<<lidar1 << std::endl;
+
+    vector<double> vout = lidar1.get_scan();
+
+    cout << lidar1 << std::endl;
+
+    cout << "Il vettore rimosso e' il seguente: "<< to_string(vout) << std::endl;
+    
+    //Costruttore di copia
+    LidarDriver lidar2(lidar1);
+    
+    cout << lidar2; //scrivi che devono essere uguali 
+
+    try
+    {
+        cout << "Provo a definire la definizione come 3.0 per verificare il corretto lancio dell'errore" << std::endl;
+        LidarDriver lidar3(3.0);
+    }
+    catch(Invalid) //capire se va bene così la classe per gestione errori
+    {
+        cout << "Errore rilevato, ridefinisco lidar3 con risoluzione 1.0" << std::endl;
+        LidarDriver lidar3(1.0)    
+        cout << lidar3 << std::endl;
+    }
+    //controllo operator=
+    lidar3 = lidar1;
+
+    cout<<lidar3; //scrivi che devono essere uguali
+
+    //Proviamo con un double
+    double dout = lidar3.get_distance(90.0);
+
+    cout << dout << std::endl;
+    //proviamo con un int
+    dout = lidar3.get_distance(10);
+
+    cout << dout << std::endl;
+    //proviamo fuori dai limiti
+    double dout = lidar3.get_distance(-1.0);
+
+    cout << dout << std::endl;
+
+    //proviamo fuori dai limiti
+    double dout = lidar3.get_distance(200);
+
+    cout << dout << std::endl;
+
+    lidar3.clear_buffer();
+
+    cout << lidar3 << std::endl;
+
+    //MANCA CONTROLLO MOVE COSTRUCTOR E MOVE ASSIGNMENT
 
    return 0;
 }
