ef51fceade6c0b74553fe78109cfc6a3991f2486
Mattia-f-4 <mattia.favretto04@gmail.com>
2024-11-26 09:53:35 +0100

    Add files via upload
    Main terminato senza commenti e cout

--- main.cpp
+++ main.cpp
@@ -23,7 +23,8 @@ int main()
     //Vettore con 11 elementi
     vector<double> v3{1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0};
 
-    
+    //Vettore con 20 elementi
+    vector<double> v4{1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0,16.0,17.0,18.0,19.0,20.0};
 
     //Costruttore 1 parametro
     LidarDriver lidar1(1.0);
@@ -91,7 +92,22 @@ int main()
 
     cout << lidar3 << std::endl;
 
-    //MANCA CONTROLLO MOVE COSTRUCTOR E MOVE ASSIGNMENT
+  
+    //Test move costructor
+    LidarDriver test(vector<double> v4)
+    {
+        LidarDriver lidar4(1.0);
+        lidar4.new_scan(v4);
 
-   return 0;
+        return v4;
+    }
+
+    LidarDirver lidar5 = test(v4);
+    cout << lidar5 << std::endl;
+
+    //Test move assignment
+    lidar2 = test(v4);
+    cout << lidar2 << std::endl;
+
+    return 0;
 }
